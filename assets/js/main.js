2e04cab7146514fc80fcee9dc1f7d7b7afef9726
serdargoleli <web.serdargoleli@gmail.com>
2021-11-29 23:47:11 +0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,30 @@
-/* show menu */
-const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-          nav    = document.getElementById(navId);
+/*=============== MOBILE MENU TOGGLE ===============*/
+const btnMobileMenu = document.getElementById("btn-menu");
+const mobileMenu = document.getElementById("menu");
 
-    if(toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
-        })
-    }
+btnMobileMenu.addEventListener("click", () => {
+  btnMobileMenu.classList.toggle("rotate-90");
+  mobileMenu.classList.toggle("menu-show");
+});
+
+/*=============== MOBILE MENU CLICK > REMOVE MOBILE MENU CLOSE ===============*/
+const navLink = document.querySelectorAll(".nav-right-menu a");
+
+function linkAction() {
+  const navMenu = document.getElementById("menu");
+  btnMobileMenu.classList.remove("rotate-90");
+  navMenu.classList.remove("menu-show");
 }
 
-showMenu('nav-toggle', 'nav-menu')
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*=============== REMOVE MENU MOBILE ===============*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show')
+/*=============== ADD ACTIVE CLASS ===============*/
+const nav_links = mobileMenu.getElementsByClassName("nav-link");
+let i = 0;
+for (i; i < nav_links.length; i++) {
+  nav_links[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
 }
-
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
